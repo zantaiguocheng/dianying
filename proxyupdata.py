844865830c39d5bf33fa797484f3f16888e63b0e
unknown <kyc1159075055@gmail.comgit config --global user.name zantaiguocheng>
2019-01-20 19:56:51 +0800

    添加注释

--- proxyupdata.py
+++ proxyupdata.py
@@ -4,5 +4,6 @@ import time
 while True:
     time.sleep(15*60)
     os.chdir('e:/pythonwj/python/pachong/proxylist')
-    os.system('dir')
+    # git fetch --all 从远端拉取最新文件
+    # git reset --hard origin/master 将hard更新为远端文件
     os.system('git fetch --all & git reset --hard origin/master')
